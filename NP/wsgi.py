ed62196fe2991c0a64e331d0e1b901da47b09584
Anurag Kumar <anuragkr.bk@gmail.com>
2019-04-10 03:30:24 +0530

    apply beautifier

--- NP/wsgi.py
+++ NP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NP project.
+WSGI config for np project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'np.settings')
 
 application = get_wsgi_application()
--- navprayas/views.py
+++ navprayas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'navprayas/home.html', {})
+    return render(request, "navprayas/home.html", {})
